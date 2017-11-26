2e84c340d579030a2e1d4d310e0a165534b104d6
Jesse Lewis <jesseaustinlewis@gmail.com>
2017-11-25 22:28:15 -0600

    adding headers

--- server.js
+++ server.js
@@ -11,7 +11,28 @@ require('dotenv').config();
 
 //Setup App
 const app = express();
-const port = process.env.PORT || 3010;
+const port = process.env.PORT || 3001;
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', process.env.REACT_HOST_URL);
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', false);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 
 //enable bodyparser for html or forms responses
 app.use(bodyParser.urlencoded({extended:false}));
@@ -33,8 +54,6 @@ app.get('/user/:userid/artworks', appRoutes.getMyArtworks);
 
 app.post('/user', appRoutes.createNewUser);
 app.post('/user/:userid/artwork', appRoutes.createNewArtwork);
-
-
 
 // RUN SERVER
 app.listen(port,function(){
